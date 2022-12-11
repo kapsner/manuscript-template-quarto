96c2c47880efc96c6073ac189d48cffb436e28d5
kapsner <lorenz.kapsner@web.de>
2022-12-11 14:40:06 +0100

    refactor: renaming of project structure, added folder for manuscript versions

--- R/00setup.R
+++ R/00setup.R
@@ -3,9 +3,9 @@ lua_base_url <- "https://raw.githubusercontent.com/kapsner/lua-filters/affiliati
 # download lua filters
 download.file(
   url = paste0(lua_base_url, "author-info-blocks/author-info-blocks-quarto.lua"),
-  destfile = here::here("30meta", "author-info-blocks-quarto.lua")
+  destfile = here::here("12meta", "author-info-blocks-quarto.lua")
 )
 download.file(
   url = paste0(lua_base_url, "scholarly-metadata/scholarly-metadata-quarto.lua"),
-  destfile = here::here("30meta", "scholarly-metadata-quarto.lua")
+  destfile = here::here("12meta", "scholarly-metadata-quarto.lua")
 )
