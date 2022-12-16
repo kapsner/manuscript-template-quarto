059d877a40d62845dc4cafd8cd8fb608aafe18cd
kapsner <lorenz.kapsner@web.de>
2022-12-16 19:02:02 +0100

    refactor: updated repo to work with new quarto extension for author block

--- 3supplement/prepare_supplement.R
+++ 3supplement/prepare_supplement.R
@@ -5,6 +5,28 @@ change_yml <- gsub("output-file: (.*)$", "output-file: Supplementary_Information
 change_yml <- gsub("fig-title: Figure", "fig-title: Figure S", change_yml)
 change_yml <- gsub("tbl-title: Table", "tbl-title: Table S", change_yml)
 
+recursive_copy <- function(old_dir, new_dir_base) {
+  cur_files <- list.files(path = old_dir, all.files = FALSE, full.names = FALSE)
+  for (f in cur_files) {
+    fn_old <- file.path(old_dir, f)
+    fn_new <- file.path(new_dir_base, f)
+    fi <- file.info(fn_old)
+    if (fi$isdir) {
+      dir.create(fn_new, recursive = TRUE)
+      recursive_copy(old_dir = fn_old, new_dir_base = fn_new)
+    } else {
+      file.copy(from = fn_old, to = fn_new)
+    }
+  }
+}
+
+ext_dir <- here::here("_extensions")
+if (file.exists(ext_dir)) {
+  
+  supp_extensions <- here::here("3supplement", "_extensions")
+  recursive_copy(old_dir = ext_dir, new_dir_base = supp_extensions)
+}
+
 generic_path_changes <- c(
   "output-dir", "bibliography", "csl",
   "scholarly-metadata-quarto.lua", "author-info-blocks-quarto.lua"
